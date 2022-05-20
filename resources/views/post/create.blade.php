ba9949cd225b6646f573830c96573e81885fb240
mariobourgeois <mariobourgeois97.200@gmail.com>
2022-05-20 09:50:44 -0400

    création blade index et blade create

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Créer un post') }}
+    </h2>
+</x-slot>
+
+
+<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+    <form action="{{route('posts.store')}}" method="post" enctype="multipart/form-data">
+    
+        <x-label for="title" value='Titre du post'/>
+        <x-input id="title" name='title'/>
+    </form>
+    
+</div>
+
+</x-app-layout>
